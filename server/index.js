1b4ba44aa4cc76fa62074c6ff79e3e7e2bbf9280
theKnightsOfRohan <shadowsovereign100@gmail.com>
2024-09-01 19:14:37 -0500

    feat: add basic websocket listeners

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('new connection');
+    socket.on('disconnect', () => {
+        console.log('client disconnect')
+    })
 });
 
 server.listen(3000, () => {
